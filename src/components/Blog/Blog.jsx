fec698bbc2677f14fb09571aa1947e43160d7490
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-09-04 17:37:42 +0545

    add a loading effect on single blog page

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Axios from "./../../api/server";
 import { useParams, Link } from "react-router-dom";
+import Loader from "./../Loader/Blog/Blog";
 import Markdown from "marked";
 import "./Blog.scss";
 
 const Blog = () => {
-  const [blog, setBlog] = useState({});
+  const [blog, setBlog] = useState(null);
   const [markdown, setMarkdown] = useState("");
   const { id } = useParams();
 
@@ -24,12 +25,18 @@ const Blog = () => {
         <Link to="/" className="link">
           <h3>Programming Blogs</h3>
         </Link>
-        <h1>{blog.title}</h1>
-        <p className="date">{blog.date}</p>
-        {blog.tag && <p className="tag">{blog.tag.name}</p>}
-        <div className="content">
-          <article dangerouslySetInnerHTML={{ __html: markdown }}></article>
-        </div>
+        {!blog ? (
+          <>
+            <h1>{blog.title}</h1>
+            <p className="date">{blog.date}</p>
+            {blog.tag && <p className="tag">{blog.tag.name}</p>}
+            <div className="content">
+              <article dangerouslySetInnerHTML={{ __html: markdown }}></article>
+            </div>
+          </>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
---
+++ src/components/Loader/Blog/Blog.jsx
@@ -0,0 +1,72 @@
+import "./Blog.scss";
+
+const Blog = () => {
+  return (
+    <>
+      <div className="blogSk">
+        <h1></h1>
+        <p className="date"></p>
+        <p className="tag"></p>
+        <div className="content">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="content">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="content">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+      <div className="blogSk">
+        <h1></h1>
+        <p className="date"></p>
+        <p className="tag"></p>
+        <div className="content">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="content">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="content">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
